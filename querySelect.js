dacbcbe3ecf212ba7cef0314c2aaa630ae2356d3
Rakeshpadare <rakeshpadare07@gmail.com>
2022-11-25 12:26:54 +0530

    first commit

---
+++ querySelect.js
@@ -0,0 +1,20 @@
+var header=document.querySelector('#main-header');
+
+header.style.borderBottom='solid 9px #000';
+
+
+
+// for input 
+
+var input=document.querySelector('input');
+
+input.value='Hello World';
+
+
+
+// for submit button
+
+var submit=document.querySelector('input[type="submit"]');
+
+submit.value='send the Message';
+
